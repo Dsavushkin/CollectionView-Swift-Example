e917e5667d5e3282a2db960dc635a6980d4ad9fe
Dmitriy Savushkin <da.savushkin4@gmail.com>
2020-03-12 15:20:37 +0300

    Create Project
    Create Project

---
+++ MenuCollection/MenuCells/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  MenuCollection
+//
+//  Created by Дмитрий Савушкин on 11.03.2020.
+//  Copyright © 2020 Дмитрий Савушкин. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
